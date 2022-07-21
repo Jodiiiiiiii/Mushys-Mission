7d9efad69e8e8431b7f0162c8920a32977b55c05
Justin Foster <33043348+JohhnSmithh@users.noreply.github.com>
2022-07-21 16:06:21 -0400

    Collectible animations
    collectible now has an idle animation that cycles color to make it twinkle every now and then. when in collecting state, it rapidly flashes and bounces. collectible prefab is now an empty object containing the collectible (so that bouncing animation can use relative position)

--- Assets/Scenes/Platformer/Scripts/Collectible.cs
+++ Assets/Scenes/Platformer/Scripts/Collectible.cs
@@ -12,6 +12,7 @@ public class Collectible : MonoBehaviour
     // components
     private CircleCollider2D circleCollider;
     private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
     // variables
     public bool isCollected { private set; get; }
@@ -27,6 +28,7 @@ public class Collectible : MonoBehaviour
         // components
         circleCollider = GetComponent<CircleCollider2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
 
         // set collectible as active or not based on whether it has already been collected
         isCollected = gameManager.GetCollectibleState(index);
@@ -82,7 +84,9 @@ public class Collectible : MonoBehaviour
 
         // disable collider and renderer
         circleCollider.enabled = false;
-        spriteRenderer.enabled = false; // start 5 second collecting animation for gem here instead of disabling sprite renderer yet
+
+        // trigger collecting animation
+        animator.SetBool("collecting", true);
     }
 
 }
